841c77851ccd1b814e2b4845c302b5aba25da4b4
mohanrajmohan1725 <mohanrajmohan1725>
2026-04-26 11:54:16 +0530

    final ecom update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ function Navbar() {
         <div className="max-w-7xl mx-auto px-4 py-3 flex justify-between items-center">
           {/* 🔥 LOGO */}
           <Link to="/" className="flex items-center gap-2">
-            <img src="public/logo.png.png" className="w-12 h-12" />
+            <img src="/logo.png" className="w-12 h-12" />
             <span className="text-xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
               MyShop
             </span>
